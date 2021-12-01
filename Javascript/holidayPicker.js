c5f42e446ba0339fc321fbb5aef0289769d49713
RileyWood938 <71392554+RileyWood938@users.noreply.github.com>
2021-11-30 21:34:17 -0500

    Got holidayPicker set up at last!
    finally set up the holiday picker script that adds some info about the next holiday closure

--- Javascript/holidayPicker.js
+++ Javascript/holidayPicker.js
@@ -1,27 +1,33 @@
+nextClosure = " ";
+
+closureSpot = document.getElementById('closureSpot');
 today = new Date();
-nextClosure = new Date ()
-christmas = new Date("12-24-21");
-ExtraDate = new Date("12-20-21");
-newYearsDay = new Date("12-31-21");
-MLKDay = new Date("01-17-22");
+months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday','Friday','Saturday'];
+/* Holidays*/
+custom1 = new Date("12-7-21");
+christmas = new Date("12-24-2021");
+newYear = new Date("12-31-21");
+mlk = new Date("1-17-22");
+washington = new Date("2-21-22");
+custom2 = new Date("3-25-22");
+memorial = new Date("5-30-22");
+juneteenth  = new Date("6-20-22");
 
-holidays = [christmas, ExtraDate, newYearsDay, MLKDay];
+holidays = [custom1, christmas,newYear, mlk, washington, memorial, juneteenth];
 
-intermediate = [];
+for (var i=0; (i > holidays.length) || (nextClosure == " "); i++) {
+	if (today.getTime()-holidays[i].getTime()<=0) {
+		nextClosure = holidays[i];
+		console.log(nextClosure);
 
-var i=0;
-
-for(i=0; i<holidays.length; i++){
-
-	if(today.getTime()-holidays[i].getTime()<0){
-		intermediate[i] = -1*(today.getTime()-holidays[i])
-	 	console.log(holidays[i] + " " + intermediate[i])
 	}
+};
 
-}
-
-min = Math.min(...intermediate);
-console.log(intermediate.indexOf(min));
-console.log(min)
-nextClosure.setDate(holidays[min]);
-console.log("next closure is: "+ nextClosure.toString())
+closureBigDay = days[nextClosure.getDay()];
+closureMonth = months[nextClosure.getMonth()];
+closureDay = nextClosure.getDate();
+closureYear = nextClosure.getFullYear();
+nextClosureDate = closureBigDay + ' ' + closureMonth + ' ' + closureDay + ', ' + closureYear ;
+closureSpot.innerHTML = 'Our next holiday closure will be: ' + nextClosureDate;
+console.log(nextClosureDate);
